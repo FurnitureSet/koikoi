ae1b71a003d6ce48d021449f1e0cc24a04f4581b
anathangd <anathangd@gmail.com>
2025-10-18 12:02:09 +0900

    Tabletop
    Resized the cards to actual size, created a table plane, tweaked the display script

--- Assets/Scripts/DebugScripts/CardDisplayDebug.cs
+++ Assets/Scripts/DebugScripts/CardDisplayDebug.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CardDisplayDebug : MonoBehaviour {
     [SerializeField] private Card cardPrefab;
     [SerializeField] private float offset_x = 0f;
     [SerializeField] private float offset_y = 0f;
+    [SerializeField, Range(0.005f, 0.1f)] private float verticalSpacing = 0.06f;
+    [SerializeField, Range(0.005f, 0.1f)] private float horizontalSpacing = 0.03f;
     [SerializeField] private Deck deck;
 
     void Start() {
@@ -20,12 +23,24 @@ public class CardDisplayDebug : MonoBehaviour {
 
     public void MoveCards() {
         deck.Shuffle();
+        const int columns = 12;
+        const int rows = 4;
         for (int i = 0; i < 48; i++) {
             Card card = deck.DrawCard();
-            int id_x = i % 12 - 6;
-            int id_y = i / 12 - 2;
-            card.transform.position = new Vector3(id_x + id_x * offset_x, id_y + id_y * offset_y);
-            card.transform.rotation = Quaternion.Euler(0, 180, 0);
+            float xIndex = (i % columns) - (columns - 1) * 0.5f;
+            float zIndex = (i / columns) - (rows - 1) * 0.5f;
+            float xPos = xIndex * horizontalSpacing + offset_x;
+            float zPos = zIndex * verticalSpacing + offset_y;
+            card.transform.position = new Vector3(xPos, 0.01f, zPos);
+            card.transform.rotation = Quaternion.Euler(-90, 180, 0);
+            card.AddComponent<Rigidbody>();
+            BoxCollider boxCollider = card.GetComponent<BoxCollider>();
+            if (boxCollider == null) {
+                boxCollider = card.AddComponent<BoxCollider>();
+            }
+            boxCollider.size = new Vector3(0.61f, 1f, 0.04f);
+            // card.transform.position = new Vector3(id_x + id_x * offset_x, id_y + id_y * offset_y);
+            // card.transform.rotation = Quaternion.Euler(0, 180, 0);
         }
     }
 }
